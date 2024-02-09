f3c1b1ff6b049e86f136313e034c0faba90b371d
Ben MacDonald <benjamin.t.macdonald1@gmail.com>
2024-02-08 23:49:42 -0500

    uploading working app

---
+++ translate_workflow.py
@@ -0,0 +1,44 @@
+from pptx import Presentation
+from transformers import pipeline
+from io import BytesIO
+
+
+
+def translate_pptx_inplace(input_file):
+    
+    # Instantiate translation pipeline
+    pipe = pipeline("translation", model="Helsinki-NLP/opus-mt-ar-en")
+
+    # Load the presentation
+    prs = Presentation(input_file)
+
+    # Helsinki model has a bug that replaces a empty prompt with this obnoxious string.
+    filler = 'Hey, hey, hey, hey, hey, hey, hey, hey, hey, hey, hey, hey, hey, hey, hey, hey, hey, hey, hey, hey, hey, hey, hey, hey, hey, hey, hey, hey, hey, hey, hey, hey, hey, hey, hey, hey, hey, hey, hey, hey, hey.'
+    
+    # For each slide in the presentation
+    for slide_number, slide in enumerate(prs.slides):  
+        
+        # For each shape in a slide
+        for shape in slide.shapes:
+    
+            # Testing the "has_text_frame" parameter
+            if shape.has_text_frame:
+                
+                # For each paragraph of the text_frame
+                for paragraph in shape.text_frame.paragraphs:
+                    
+                    # Send paragraphs through translation pipeline
+                    results = pipe(paragraph.text)
+                    text_to_add = results[0]['translation_text']
+                    text_fixed = text_to_add.replace(filler, '')
+                    paragraph.text = text_fixed
+                    binary_output = BytesIO()
+
+    # save new file with translations
+    return prs.save(binary_output)
+
+if __name__ == "__main__":
+    
+    file_name = './Presentation1.pptx'
+    new_file = 'test3.pptx'
+    texts = translate_pptx_inplace(file_name)
--- translator_app.py
+++ translator_app.py
@@ -1,15 +1,28 @@
+import streamlit as st # pip install streamlit==0.82.0
 from pptx import Presentation
 from transformers import pipeline
+from io import BytesIO
 
 
+st.set_page_config(page_title='Powerpoint Translator', layout='wide', initial_sidebar_state='expanded')
 
-def translate_pptx_inplace(input_file, output_file):
-    
+
+st.title('Powerpoint Translator')
+
+
+uploaded_file = st.file_uploader('upload your powerpoint file here')
+
+if uploaded_file:
+   filename = st.write("Filename: ", uploaded_file.name)
+   out_name = uploaded_file.name.replace('.pptx', '')
+
+if st.button('Translate powerpoint'):
+        
     # Instantiate translation pipeline
     pipe = pipeline("translation", model="Helsinki-NLP/opus-mt-ar-en")
 
     # Load the presentation
-    prs = Presentation(input_file)
+    prs = Presentation(uploaded_file)
 
     # Helsinki model has a bug that replaces a empty prompt with this obnoxious string.
     filler = 'Hey, hey, hey, hey, hey, hey, hey, hey, hey, hey, hey, hey, hey, hey, hey, hey, hey, hey, hey, hey, hey, hey, hey, hey, hey, hey, hey, hey, hey, hey, hey, hey, hey, hey, hey, hey, hey, hey, hey, hey, hey.'
@@ -31,12 +44,14 @@ def translate_pptx_inplace(input_file, output_file):
                     text_to_add = results[0]['translation_text']
                     text_fixed = text_to_add.replace(filler, '')
                     paragraph.text = text_fixed
-                    
-    # save new file with translations
-    return prs.save(output_file)
+    binary_output = BytesIO()
+# save new file with translations
+    prs.save(binary_output)
 
-if __name__ == "__main__":
-    
-    file_name = './Presentation1.pptx'
-    new_file = 'test2.pptx'
-    texts = translate_pptx_inplace(file_name, new_file)
+
+    st.download_button(label='Click to download PowerPoint',data=binary_output.getvalue(),file_name=f'{out_name}-translated.pptx')
+
+
+
+ 
+ 
